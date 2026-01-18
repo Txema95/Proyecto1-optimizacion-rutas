da3cc6dde72190a25282d0062ac83cecee7aef10
PolPanyella <pol.panyella@enti.cat>
2026-01-18 19:41:07 +0100

    mapa funcional

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import streamlit as st
 from streamlit_folium import st_folium
+import openrouteservice
+
+
+# Conexion con openroute para calcular rutas
+OPENROUTER_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijk5MjU0MTEzN2M4ODRiYjM5YzkyODFlNWRjZDRlOWY0IiwiaCI6Im11cm11cjY0In0="
+client = openrouteservice.Client(key=OPENROUTER_API_KEY)
+
+
 
 #Conectar archivo base de datos
 import os
@@ -37,21 +45,32 @@ def limpiar_columnas(df):
 def main():
 
     #Creamos el objeto de la base de datos
-    db= database()
     
-    
+    # db= database()
     st.set_page_config(page_title="Gestor de rutas", layout="wide")
+    
+    
+    # ----------------- LAYOUT -----------------
+    st.title("Proyecto 1: Optimizacion de Rutas")
+
+    col_left, col_middle, col_right = st.columns([1, 2, 1])
+
+    
+    
+    
+    
+    
 
     # ----------------- CARGAR DATOS -----------------
-    pedidos = limpiar_columnas(db.select_all('Pedidos'))
-    clientes = limpiar_columnas(db.select_all('Clientes'))
-    destinos = limpiar_columnas(db.select_all('Destinos'))
-    lineaspedidos = limpiar_columnas(db.select_all('LineasPedido'))
-    productos = limpiar_columnas(db.select_all('Productos'))
-    provincias = limpiar_columnas(db.select_all('Provincias'))
+    pedidos = limpiar_columnas(pd.read_csv("../app/data/pedidos.csv", sep=";"))
+    clientes = limpiar_columnas(pd.read_csv("../app/data/clientes.csv", sep=";"))
+    destinos = limpiar_columnas(pd.read_csv("../app/data/destinos.csv", sep=";"))
+    lineaspedidos = limpiar_columnas(pd.read_csv("../app/data/lineaspedidos.csv", sep=";"))
+    productos = limpiar_columnas(pd.read_csv("../app/data/productos.csv", sep=";"))
+    provincias = limpiar_columnas(pd.read_csv("../app/data/provincias.csv", sep=";"))
     
-    # info a mostrar
     '''
+    # info a mostrar
         Id pedido
         FechaPedido
         Productos
@@ -61,9 +80,8 @@ def main():
             Precio
         Cliente
         Nombre destino
-    '''
     print(pedidos.columns)
-    print(clientes.columns)
+    print(clientes.columns)'''
 
 
     
@@ -86,15 +104,13 @@ def main():
     grupos = detalle.groupby("PedidoID")
 
 
-    # ----------------- LAYOUT -----------------
-    st.title("Proyecto 1: Optimizacion de Rutas")
-
-    col_left, col_middle, col_right = st.columns([1, 2, 1])
-
+    
     # -------- COLUMNA IZQUIERDA (Pedidos) --------
+    
     with col_left:
         st.subheader("Lista de pedidos")
         st.markdown("---")
+        
 
 
         
@@ -131,7 +147,7 @@ def main():
                     <b>{r['Nombre']}:</b></summary>
                     <ul>
                 """
-                precioTotalProducto = float(r["PrecioVenta"]) * float(r["Cantidad"])
+                precioTotalProducto = float(r["PrecioVenta"].replace(",", ".")) * float(r["Cantidad"])
                 productos_html += f"<li><b>Cantidad: </b>{r['Cantidad']}</li>"
                 productos_html += f"<li><b>Precio total: </b>{precioTotalProducto}€</li>"
                 productos_html += f"<li><b>Dias en caducar: </b>{r['Caducidad']}</li>"
@@ -171,18 +187,125 @@ def main():
         lista_html += "</div>"
 
         st.markdown(lista_html, unsafe_allow_html=True)
+        
 
 
     # -------- COLUMNA MEDIO (mapa) --------
     with col_middle:
         st.title("Mapa de rutas")
+        
 
-        m = folium.Map(location=[41.3874, 2.1686], zoom_start=12)
-        folium.Marker([41.3874, 2.1686], popup="Barcelona").add_to(m)
 
+        
+        
+        ubicaciones = [
+            {"nombre": "Barcelona", "lat": 41.3874, "lon": 2.1686},
+            {"nombre": "Madrid",    "lat": 40.4168, "lon": -3.7038},
+            {"nombre": "Valencia",  "lat": 39.4699, "lon": -0.3763},
+        ]
+        
+        coords = [(c["lon"], c["lat"]) for c in ubicaciones]
+
+        
+
+        route = client.directions(
+            coordinates=coords,
+            profile="driving-car",
+            format="geojson"
+        )
+
+        # ORS devuelve geometry en (lon, lat). Folium necesita (lat, lon).
+        line_lonlat = route["features"][0]["geometry"]["coordinates"]
+        line_latlon = [(lat, lon) for lon, lat in line_lonlat]
+
+        # Centro del mapa
+        start_lat, start_lon = line_latlon[0]
+        m = folium.Map(location=[start_lat, start_lon], zoom_start=13)
+        
+        points_latlon = [(lat, lon) for lon, lat in coords]
+
+        for i, p in enumerate(points_latlon):
+            folium.Marker(
+                location=p,
+                popup= ubicaciones[i]["nombre"],
+                icon=folium.Icon(color="blue" if i not in (0, len(points_latlon)-1) else "red")
+            ).add_to(m)
+        
+
+        # Marcadores inicio/fin
+        # folium.Marker(line_latlon[0], popup="Inicio").add_to(m)
+        # folium.Marker(line_latlon[-1], popup="Fin").add_to(m)
+
+        # Ruta
+        folium.PolyLine(line_latlon, weight=5, opacity=0.8).add_to(m)
+
+        summary = route["features"][0]["properties"]["summary"]
+
+        km = summary["distance"] / 1000
+
+        
+        badge = f"""
+        <div style="
+            position: fixed;
+            top: 20px; left: 20px;
+            z-index: 9999;
+            font-size: 28px;
+            font-weight: 800;
+            background: rgba(255,255,255,0.6);
+            padding: 6px 10px;
+            border-radius: 10px;
+            ">
+            Km: {km:.2f}
+            </div>
+            """
+        m.get_root().html.add_child(folium.Element(badge))
         st_folium(m, width=700, height=500)
         
         
+        
+        '''
+        
+
+        m = folium.Map(location=[41.3874, 2.1686], zoom_start=12)
+
+        
+        
+        
+        
+        
+        puntos = [
+            [41.3874, 2.1686],  # Barcelona
+            [41.3809, 2.1228],  # ejemplo punto 2
+            [41.4036, 2.1744],  # ejemplo punto 3
+        ]
+
+        m = folium.Map(location=puntos[0], zoom_start=12)
+
+        # marcadores (opcional)
+        for p in puntos:
+            folium.Marker(p).add_to(m)
+
+        # la ruta (línea)
+        folium.PolyLine(puntos, weight=5, opacity=0.8).add_to(m)
+
+        m.save("ruta.html")
+        
+        
+        folium.Marker([41.3874, 2.1686], popup="Barcelona").add_to(m)
+
+        st_folium(m, width=700, height=500)
+        '''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
         # Contenedor grande (mapa / visualización de ruta)
         st.markdown(
             """
@@ -201,7 +324,7 @@ def main():
             </div>
             """,
             unsafe_allow_html=True,
-        )
+        )'''
 
 
 
